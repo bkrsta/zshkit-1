c6babc5c7063bde4d7228246ed8ab952e5f22c54
BKrsta <bkrsta@gmail.com>
2009-07-25 14:27:08 +0200

    - Added aliases: gitx (/Applications/GitX.app/Contents/Resources/gitx), ss (./script/server) - Changed prompt

--- 02_alias.sh
+++ 02_alias.sh
@@ -3,3 +3,5 @@ alias su='sudo zsh'
 alias c='clear'
 
 alias gitnub="/Applications/GitNub.app/Contents/MacOS/GitNub"
+alias gitx="/Applications/GitX.app/Contents/Resources/gitx"
+
--- 10_prompt.sh
+++ 10_prompt.sh
@@ -1,18 +1,34 @@
 setopt prompt_subst
 
+# PS_ERROR="%(?::%{$fg_bold[white]$bg[red]%}[%?]%{$reset_color%} )"
+# PS_USER="%(#:%{$fg[red]%}:%{$fg[cyan]%})%n%{$reset_color%}"
+# PS_HOST="@%{$fg[cyan]%}%m%{$reset_color%}"
+# PS_DIR=":%{$fg[cyan]%}%~%{$reset_color%}"
+# PS_ROOT_HASH="%{[0m%}%(#:%{[0;31m%}#%{[0m%}:)"
+
 PS_ERROR="%(?::%{$fg_bold[white]$bg[red]%}[%?]%{$reset_color%} )"
-PS_USER="%(#:%{$fg[red]%}:%{$fg[cyan]%})%n%{$reset_color%}"
-PS_HOST="@%{$fg[cyan]%}%m%{$reset_color%}"
-PS_DIR=":%{$fg[cyan]%}%~%{$reset_color%}"
+PS_USER="%(#:%{$fg[red]%}:%{$fg[green]%})%n%{$reset_color%}"
+PS_HOST="%{$fg[cyan]%}@%{$fg[cyan]%}%m%{$reset_color%}"
+PS_DIR=":%{$fg[red]%}%~%{$reset_color%}"
 PS_ROOT_HASH="%{[0m%}%(#:%{[0;31m%}#%{[0m%}:)"
+
+# PROMPT='\
+# %{$reset_color%}\
+# $PS_ERROR\
+# $PS_USER\
+# $PS_HOST\
+# $PS_DIR\
+# $PS_GIT\
+# $(git-prompt)\
+# $PS_ROOT_HASH\
+#  # '
 
 PROMPT='\
 %{$reset_color%}\
-$PS_ERROR\
 $PS_USER\
 $PS_HOST\
 $PS_DIR\
 $PS_GIT\
 $(git-prompt)\
 $PS_ROOT_HASH\
- '
+ #→ '
