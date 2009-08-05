fdba9756325aae7e6d83a96e07b48ccbf3f81b26
BKrsta <bkrsta@gmail.com>
2009-08-05 21:29:10 +0200

    git-cola alias

--- 02_alias.sh
+++ 02_alias.sh
@@ -4,6 +4,7 @@ alias c='clear'
 
 alias gitnub="/Applications/GitNub.app/Contents/MacOS/GitNub"
 alias gitx="/Applications/GitX.app/Contents/Resources/gitx"
+alias gcola="/Applications/git-cola.app/Contents/MacOS/git-cola"
 
 #export GIT_EDITOR="mate -w"
 
